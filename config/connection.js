94a11db5f14fd34d44af8282cd11f8ce2d79b3f8
Julie Schaub <julie.schaub@hotmail.com>
2020-09-07 16:35:45 -0500

    updated connection file

--- config/connection.js
+++ config/connection.js
@@ -1,23 +1,25 @@
 /* Use this file to setup code to connect node to mySQL
 Export connection*/
 
-const mysql = require('mysql');
+//Set up MySQL connection.
+var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-    host: "localhost",
-    port: 0,
-    user: "root",
-    password: "Tbjs233069$",
-    database: "burgers_db"
-  });
-  
-  connection.connect(function(err) {
-    if (err) {
-      console.error("error connecting: " + err.stack);
-      return;
-    }
-  
-    console.log("connected as id " + connection.threadId);
-  });
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "Tbjs233069$",
+  database: "burgers_db"
+});
 
-  module.exports = connection;
+// Make connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+// Export connection for our ORM to use.
+module.exports = connection;
