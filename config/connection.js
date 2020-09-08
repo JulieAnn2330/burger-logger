5a439de1ad722ae0e6d9f0df3d6d669274bbbe5d
Julie Schaub <julie.schaub@hotmail.com>
2020-09-08 14:01:38 -0500

    updated connection

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ var mysql = require("mysql");
 
 var connection = mysql.createConnection({
   host: "127.0.0.1",
-  port: 3307,
+  port: 3306,
   user: "root",
   password: "Tbjs233069$",
   database: "burgers_db"
