2d9ff4e59a748dc4a807ae6ce4863451223a3d24
Julie Schaub <julie.schaub@hotmail.com>
2020-09-08 14:07:26 -0500

    connection

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,7 @@ var mysql = require("mysql");
 if (process.env.NODE_ENV === 'production') {
   const connection = mysql.createConnection(process.env.JAWSDB_URL)
 } else {
- var connection = mysql.createConnection({
+ const connection = mysql.createConnection({
   host: "127.0.0.1",
   port: 3306,
   user: "root",
